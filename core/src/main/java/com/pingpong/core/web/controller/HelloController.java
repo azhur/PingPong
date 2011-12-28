ba8dc704bdd55af996f6ada002fa160fa716fb4f
azhurat <artur.zhurat@uwiss.com>
2011-12-28 23:12:03 +0200

    refactoring? add Controller annotation;

--- core/src/main/java/com/pingpong/core/web/controller/HelloController.java
+++ core/src/main/java/com/pingpong/core/web/controller/HelloController.java
@@ -5,13 +5,8 @@ package com.pingpong.core.web.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Artur Zhurat
@@ -19,16 +14,14 @@ import java.io.IOException;
  * @since 27/12/2011
  */
 
-public class HelloController implements Controller {
+@Controller
+public class HelloController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
+	@RequestMapping("hello.jsp")
+    public void set(){
+		System.out.println("Returning hello view");
 		LOGGER.info("Returning hello view");
-
-        return new ModelAndView("hello.jsp");
-    }
-
+	}
 }
